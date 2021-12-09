a7f80780170cdfad776f83bce31523d10032ff03
codewdy <codewdy@gmail.com>
2021-12-10 07:36:39 +0800

    use ray-x signature due to the bug fix is merged

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -1,4 +1,4 @@
 local pkg = require('pkg')
 pkg.req({'codewdy/clangd-helper.nvim'})
 pkg.req({'neovim/nvim-lspconfig', config=[[require("lsp/nvim-lspconfig")]]})
-pkg.req({'codewdy/lsp_signature.nvim', config=[[require("lsp/lsp_signature")]]})
+pkg.req({'ray-x/lsp_signature.nvim', config=[[require("lsp/lsp_signature")]]})
